ee1a6d6f5aa78379e858e9fac50f1ab8a708423f
PrasannaPusuluri <prasu456.pusuluri@gmail.com>
2021-01-15 14:23:04 +0530

    server side code

---
+++ server/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+let userSchema = new mongoose.Schema({
+  name : {type : String , required : true},
+  email : {type : String , required : true , unique : true},
+  password : {type : String , required : true},
+  created : {type : Date , default : Date.now()},
+});
+let User = mongoose.model('user' , userSchema);
+module.exports = User;
